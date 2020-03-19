4f992c59e8a939d5f47e0b0108134c1d8c25fd31
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-19 16:28:02 -0300

    init: starts project to study redux in react native

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </>
+  );
+}
