29ab9ddf8085c03810fa9470a0a8687ecc84a54c
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-21 15:21:21 -0300

    feat: redux settings

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 import './config/reactotronConfig';
 
 import Routes from './routes';
 
+import store from './store';
+
 export default function App() {
     return (
         <>
             <StatusBar barStyle="light-content" backgroundColor="#141419" />
-            <Routes />
+            <Provider store={store}>
+                <Routes />
+            </Provider>
         </>
     );
 }
