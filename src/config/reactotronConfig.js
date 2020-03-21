a3d14d88b626ca8a7b6a4b1cd76a22d5c62436c3
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-21 16:48:45 -0300

    chore: added reactotron-redux

--- src/config/reactotronConfig.js
+++ src/config/reactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactrotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 
 if (__DEV__) {
     const tron = Reactrotron.configure({ host: '192.168.0.8' })
+        .use(reactotronRedux())
         .useReactNative()
         .connect();
 
     console.tron = tron;
 
     tron.clear();
-};
+}
